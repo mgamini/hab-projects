ed54dca548365357aa3d44bc5497e28e433b8544
Garrett Amini <garrett@chef.io>
2017-08-22 09:12:53 +0200

    init

---
+++ hab-nginx/site/habitat/plan.sh
@@ -0,0 +1,13 @@
+pkg_name="hello-hab-site"
+pkg_origin="mgamini"
+pkg_version="0.1.0"
+pkg_maintainer="Garrett Amini <kadjar@gmail.com>"
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}/dist"
+  cp -v index.html "${pkg_prefix}/dist/"
+}
