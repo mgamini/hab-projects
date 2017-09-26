63a8543052d35d1c71c00b2609887e8f8433cda5
Garrett Amini <kadjar@gmail.com>
2017-09-26 15:52:28 -0400

    attempting spa

---
+++ expresso/habitat/plan.sh
@@ -0,0 +1,4 @@
+pkg_origin=mgamini
+pkg_name=expresso
+pkg_version=0.1.0
+pkg_scaffolding=core/scaffolding-node
---
+++ node-scaffold/habitat/plan.sh
@@ -0,0 +1,4 @@
+pkg_name=node_scaffold_test
+pkg_origin=mgamini
+pkg_version=0.0.1
+pkg_scaffolding=core/scaffolding-node
